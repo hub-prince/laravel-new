dd569ffadbdf9eff7b818debff3d41f271ba1d02
hub-prince <220170107066@vgecg.ac.in>
2026-03-19 11:21:54 +0530

    role based dashboard view

--- myApp/resources/views/dashboard.blade.php
+++ myApp/resources/views/dashboard.blade.php
@@ -1,17 +1,11 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+ <x-app-layout>
+    <div class="w-[80%] mx-40 px-2 py-6 flex justify-between">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+        <h1 class="text-blue-500 text-lg mb-4">
+            Hello, User 👋
+        </h1>
+
+       
+
     </div>
 </x-app-layout>
